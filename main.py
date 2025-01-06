697506f4bbe7625f93c3878695fdb64964ec4a6b
yo-linux <nixon2045@gmail.com>
2025-01-05 21:51:35 -0500

    terminado y respondiendo endpoit PUT para actualizar tareas

--- main.py
+++ main.py
@@ -44,3 +44,16 @@ def get_task(task_id: int):
         if task["id"] == task_id:
             return task
     raise HTTPException(status_code=404, detail="Task not found")
+
+
+#endpoint para actualizar una tarea 
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int,updated_task: Task):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["completed"] =updated_task.completed
+            return task
+    raise HTTPException(status_code=404, detail="task not found")
+
