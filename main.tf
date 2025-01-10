216d07b2f9f3eba6b654c5a50581bb7c695c3528
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2025-01-10 17:06:39 -0500

    cambios en la contenerizacion en terraform

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.15.0"
+    }
+  }
+}
+
+backend "azurerm" {
+  resource_gruop_name   = "tf_backend_rg"
+  storage_account_name  = "tfstateaccount"
+  container_name        = "tfstate"
+  key                   = "terraform.tfstate"
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_group_name" "example for API" {
+    name    =   "todo-api-rg"
+    location=   "East US"
+}
+
+resource "azurerm_app_service_plan" "example for app service" {
+    name                =   "todo-api-service-plan"
+    location            =   "azurerm_group_name.example for api.location"
+    resource_gruop_name =   "azurerm_resource_group.example for API.name"
+    sku {
+        tier = "Basic"
+        size = "B1"
+    }
+}
+
+resource "azurerm_app_service" "appservice" {
+    name                =   "todo-api-service-plan"
+    location            =   "azurerm_group_name.example for api.location"
+    resource_gruop_name =   "azurerm_resource_group.example for API.name"
+    app_service_plan_id =
+}
