171f08e8248589918c1363afb293e8ccf5b8e286
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2025-01-10 17:26:56 -0500

    se finaliza contenerizacion de azure para la api

--- main.tf
+++ main.tf
@@ -37,5 +37,10 @@ resource "azurerm_app_service" "appservice" {
     name                =   "todo-api-service-plan"
     location            =   "azurerm_group_name.example for api.location"
     resource_gruop_name =   "azurerm_resource_group.example for API.name"
-    app_service_plan_id =
+    app_service_plan_id =   "azurerm_app_service_plan.example for app service.id"
+
+    site_config {
+        always_on = true 
+        linux_fx_version = "DOCKER|nox2045/todo-list-api"
+    }
 }
