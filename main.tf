2b1daaa1596826907df251adf962c4222c9a80e1
yo-linux <nixon2045@gmail.com>
2025-01-13 21:44:56 -0500

    se realiza commit

--- main.tf
+++ main.tf
@@ -2,30 +2,32 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.15.0"
+      version = ">3.0"
     }
   }
-}
+
 
 backend "azurerm" {
-  resource_gruop_name   = "tf_backend_rg"
+  resource_group_name   = "tf_backend_rg"
   storage_account_name  = "tfstateaccount"
   container_name        = "tfstate"
   key                   = "terraform.tfstate"
+ 
+  }
 }
 
 provider "azurerm" {
     features {}
 }
 
-resource "azurerm_group_name" "example for API" {
+resource "azurerm_group_name" "exampleforAPI" {
     name    =   "todo-api-rg"
     location=   "East US"
 }
 
-resource "azurerm_app_service_plan" "example for app service" {
+resource "azurerm_app_service_plan" "exampleforappservice" {
     name                =   "todo-api-service-plan"
-    location            =   "azurerm_group_name.example for api.location"
+    location            =   "azurerm_group_name.exampleforAPI.location"
     resource_gruop_name =   "azurerm_resource_group.example for API.name"
     sku {
         tier = "Basic"
